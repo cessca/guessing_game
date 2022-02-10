9cc751ad77d08b4dcc2c52443936b8132c3d6244
cessca <cessca.vr@gmail.com>
2022-02-10 18:43:51 +0900

    tutorial complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use core::num;
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
@@ -7,23 +8,31 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("秘密の数字は{}", secret_number);
+    // println!("秘密の数字は{}", secret_number);
 
-    println!("予想を入力してください");
+    loop {
+        println!("予想を入力してください");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("行の読み込みに失敗しました");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("行の読み込みに失敗しました");
 
-    let guess: u32 = guess.trim().parse().expect("数字を入力してください");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("あなたの予想は:{}", guess);
+        println!("あなたの予想は:{}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("小さすぎるよ"),
-        Ordering::Greater => println!("大きすぎるよ"),
-        Ordering::Equal => println!("正解！！"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さすぎるよ"),
+            Ordering::Greater => println!("大きすぎるよ"),
+            Ordering::Equal => {
+                println!("正解！！");
+                break;
+            }
+        }
     }
 }
